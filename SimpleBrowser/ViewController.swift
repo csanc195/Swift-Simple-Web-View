0c7c7d9ced115d12bf6c1a10f8d8f1e8e595dcac
Carlos Sanchez <CarlosSanchez@Carloss-MacBook-Pro-3.local>
2017-02-01 22:48:18 -0500

    First commit

--- SimpleBrowser/ViewController.swift
+++ SimpleBrowser/ViewController.swift
@@ -10,9 +10,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var webView: UIWebView!
+    @IBOutlet weak var navigationTitle: UINavigationBar!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        if let myURL: URL = URL(string: "https://www.youtube.com"){
+            let newURLRequest: URLRequest = URLRequest(url: myURL)
+            webView.loadRequest(newURLRequest)
+            navigationTitle.topItem?.title = myURL.host
+        }
+
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +28,24 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func goBack(_ sender: UIBarButtonItem) {
+        if webView.canGoBack{
+            webView.goBack()
+        }
+    }
+    
+    @IBAction func goForward(_ sender: UIBarButtonItem) {
+        if webView.canGoForward{
+            webView.goForward()
+        }
+    }
+    
+    @IBAction func reloadPage(_ sender: UIBarButtonItem) {
+        webView.reload()
+    }
+    
+    @IBAction func stopLoading(_ sender: UIBarButtonItem) {
+        webView.stopLoading()
+    }
 }
 
